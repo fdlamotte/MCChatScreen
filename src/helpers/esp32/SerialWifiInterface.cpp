e609ea4663ef28d92066509a18f3b89028cae00c
Florent de Lamotte <florent@frizoncorrea.fr>
2025-03-03 11:38:20 +0100

    strenghten com with header for frame size

--- src/helpers/esp32/SerialWifiInterface.cpp
+++ src/helpers/esp32/SerialWifiInterface.cpp
@@ -6,27 +6,50 @@ void WiFiEvent(WiFiEvent_t event);
 IPAddress serverIp(192, 168, 1, 1);
 IPAddress NMask(255, 255, 255, 0);
 
-#define PWD "mypasswd"
-#define SSID "myssid"
+//#define AP_SSID "myssid"
+//#define AP_PWD "mypasswd"
+
+#define NET_SSID "P76"
+#define NET_PWD "salle006@p76"
+
+void WiFiEvent(WiFiEvent_t event){
+    switch(event) {
+      case SYSTEM_EVENT_STA_GOT_IP:
+          //When connected set 
+          Serial.print("WiFi connected! IP address: ");
+          Serial.println(WiFi.localIP());
+          break;
+      case SYSTEM_EVENT_STA_DISCONNECTED:
+          Serial.println("WiFi lost connection");
+          break;
+    }
+}
 
 void SerialWifiInterface::begin() {
   IPAddress IP;
+
+  WiFi.onEvent(WiFiEvent);
+
+ #ifdef AP_SSID
   Serial.println("Setting up wifi network : " + String(SSID));
- 
+
   // Connect to Wi-Fi network with SSID and password
   Serial.println("Setting AP (Access Point)…");
   // Remove the password parameter, if you want the AP (Access Point) to be open
   WiFi.mode(WIFI_AP);
-  WiFi.softAP(SSID, PWD);
+  WiFi.softAP(AP_SSID, AP_PWD);
   Serial.println("Wait 100 ms for AP_START...");
   delay(100);
  
-  Serial.println("Set softAPConfig");
+   Serial.println("Set softAPConfig");
   WiFi.softAPConfig(serverIp, serverIp, NMask);
  
   IP = WiFi.softAPIP();
   Serial.print("AP IP address: ");
   Serial.println(IP);
+#else
+  WiFi.begin(NET_SSID, NET_PWD);
+#endif
 
   server.begin();
 }
@@ -65,13 +88,11 @@ size_t SerialWifiInterface::writeFrame(const uint8_t src[], size_t len) {
   return 0;
 }
 
-#define  SER_WRITE_MIN_INTERVAL   20
-
+#define  SER_WRITE_MIN_INTERVAL   0
 
 bool SerialWifiInterface::isWriteBusy() const {
   return millis() < _last_write + SER_WRITE_MIN_INTERVAL;   // still too soon to start another write?
 }
-
 
 size_t SerialWifiInterface::checkRecvFrame(uint8_t dest[]) {
   if (isWriteBusy())
@@ -93,8 +114,16 @@ size_t SerialWifiInterface::checkRecvFrame(uint8_t dest[]) {
 
   if (deviceConnected) {
     if (send_queue_len > 0) {   // first, check send queue
+      
       _last_write = millis();
-      client.write(send_queue[0].buf, send_queue[0].len);
+      int len = send_queue[0].len;
+
+      uint8_t pkt[3+len]; // use same header as serial interface so client can delimit frames
+      pkt[0] = '>';
+      pkt[1] = (len & 0xFF);  // LSB
+      pkt[2] = (len >> 8);    // MSB
+      memcpy(&pkt[3], send_queue[0].buf, send_queue[0].len);
+      client.write(pkt, 3 + len);
       send_queue_len--;
       for (int i = 0; i < send_queue_len; i++) {   // delete top item from queue
         send_queue[i] = send_queue[i + 1];
@@ -102,8 +131,10 @@ size_t SerialWifiInterface::checkRecvFrame(uint8_t dest[]) {
     } else {
       int len = client.available();
       if (len > 0) {
-        client.readBytes(dest, len);
-        return len;
+        uint8_t buf[MAX_FRAME_SIZE + 4];
+        client.readBytes(buf, len);
+        memcpy(dest, buf+3, len-3); // remove header (don't even check ... problems are on the other dir)
+        return len-3;
       }
     }
   }
